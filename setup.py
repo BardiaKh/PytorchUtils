fdecc10a59cf4c543daad1317448627ac84343c5
Bardia Khosravi <bardiakhosravi95@gmail.com>
2021-06-18 12:09:51 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     #long_description=long_description,
     #long_description_content_type="text/markdown",
     url="https://github.com/BardiaKh/PytorchUtils",
-    packages=['bkh_pytorch_utils'],
+    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
