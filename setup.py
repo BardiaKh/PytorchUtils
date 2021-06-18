c1dc24e3b4b2b83a7530eec5d03401e5773866d4
Bardia Khosravi <bardiakhosravi95@gmail.com>
2021-06-18 11:08:50 +0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="BardiaKh",
+    version="0.0.1",
+    author="Bardia Khosravi",
+    #author_email="bardiakhosravi95@gmail.com",
+    #description="<Template Setup.py package>",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="<https://github.com/BardiaKh/PytorchUtils",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
