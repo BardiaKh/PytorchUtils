112f7ac1ccc5e814da362ea12e1b4a0e5f77d9f4
Bardia Khosravi <bardiakhosravi95@gmail.com>
2021-07-12 18:25:39 +0430

    Update utils.py

--- bkh_pytorch_utils/pl/utils.py
+++ bkh_pytorch_utils/pl/utils.py
@@ -43,7 +43,10 @@ class BKhModule(pl.LightningModule):
         for callback in callbacks:
             if isinstance(callback, pl.callbacks.ModelCheckpoint):
                 best_weight_path=callback.best_model_path
-                self.load_from_checkpoint(best_weight_path)
+                if best_weight_path.endswith(".ckpt")
+                    self.load_from_checkpoint(best_weight_path)
+                else:
+                    raise Exception("No valid checkpoint file is found.")
 
                 break
         return
