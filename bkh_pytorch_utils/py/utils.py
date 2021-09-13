953c538430c608e8271173215f5aac3f6670ec90
Bardia Khosravi <bardiakhosravi95@gmail.com>
2021-09-13 17:09:44 -0500

    v0.0.14
    ADDED:
    -Support for label smoothing the multi-label tasks

--- bkh_pytorch_utils/py/utils.py
+++ bkh_pytorch_utils/py/utils.py
@@ -152,10 +152,14 @@ def one_hot_encode(true_labels: torch.Tensor, classes: int, smoothing=0.0):
     assert 0 <= smoothing < 1
     confidence = 1.0 - smoothing
     label_shape = torch.Size((true_labels.size(0), classes))
-    with torch.no_grad():
-        true_dist = torch.empty(size=label_shape, device=true_labels.device)
-        true_dist.fill_(smoothing / (classes - 1))
-        true_dist.scatter_(1, true_labels.data.unsqueeze(1), confidence)
+    if label_shape == true_labels.size():
+        with torch.no_grad():
+            true_dist = torch.where(true_labels==1.0, confidence, smoothing)
+    else:
+        with torch.no_grad():
+            true_dist = torch.empty(size=label_shape, device=true_labels.device)
+            true_dist.fill_(smoothing / (classes - 1))
+            true_dist.scatter_(1, true_labels.data.unsqueeze(1), confidence)
 
     return true_dist
 
