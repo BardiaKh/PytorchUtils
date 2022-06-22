a9fdaf026bf493a7e625572dcc2674760410f76c
Bardia Khosravi <bardiakhosravi95@gmail.com>
2022-06-22 15:37:47 -0500

    fixed ema device issue

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="bkh_pytorch_utils",
-    version="0.2.2",
+    version="0.2.3",
     author="Bardia Khosravi",
     author_email="bardiakhosravi95@gmail.com",
     description="A rapid prototyping tool for MONAI & PyTorch Lightning",
