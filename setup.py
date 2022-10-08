59e5450fd8d87965bde4d2d6d842685f18620576
Bardia Khosravi <bardiakhosravi95@gmail.com>
2022-10-07 19:55:06 -0500

    fixed prefetch_factor

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="bkh_pytorch_utils",
-    version="0.3.3",
+    version="0.3.4",
     author="Bardia Khosravi",
     author_email="bardiakhosravi95@gmail.com",
     description="A rapid prototyping tool for MONAI & PyTorch Lightning",
