9cdad6cb0e67c3f112023a9b5ae977f97ec4de41
Bardia Khosravi <bardiakhosravi95@gmail.com>
2023-09-13 10:33:52 -0500

    updated dataset mean_std calculation algorithm

--- bkh_pytorch_utils/py/utils.py
+++ bkh_pytorch_utils/py/utils.py
@@ -37,25 +37,42 @@ def seed_all(seed:int) -> None:
     pl.seed_everything(seed,workers=True)
 
 def get_data_stats(dataset:torch.utils.data.Dataset, img_key:str, num_channels:int = 1)->None:
-    pixels_sum=torch.zeros(num_channels)
-    pixels_count=torch.zeros(num_channels)
-    sum_squared_err=torch.zeros(num_channels)
+    #########################################################################################################
+    ### Adapted from: https://github.com/Nikronic/CoarseNet/blob/master/utils/preprocess.py#L142-L200
+    #########################################################################################################
+    """
+    Compute dataset mean and standard deviation in an online manner.
 
-    for i,b in enumerate(tqdm(dataset)):
+    Args:
+    - dataset: a PyTorch Dataset object
+    - img_key: the key to extract the image tensor from the sample
+    - num_channels: number of image channels
+
+    Returns: None (prints mean and std)
+    """
+    
+    # Initializations
+    cnt = 0
+    fst_moment = torch.zeros(num_channels)
+    snd_moment = torch.zeros(num_channels)
+
+    for b in tqdm(dataset, desc="Computing mean"):
         image = b[img_key]
-        pixels_sum = pixels_sum+image.sum((1,2))
-        pixels_count = pixels_count+torch.tensor([image.shape[1]*image.shape[2]]*num_channels)
+        b, h, w = image.shape[0], image.shape[2], image.shape[3]
+        nb_pixels = b * h * w
+        sum_ = image.sum(dim=[0, 2, 3])
+        sum_of_square = (image**2).sum(dim=[0, 2, 3])
+        
+        fst_moment = (cnt * fst_moment + sum_) / (cnt + nb_pixels)
+        snd_moment = (cnt * snd_moment + sum_of_square) / (cnt + nb_pixels)
 
-    mean = pixels_sum/pixels_count
+        cnt += nb_pixels
 
-    for i,b in enumerate(tqdm(dataset)):
-        image = b[img_key].reshape(-1,num_channels)
-        sum_squared_err = sum_squared_err + ((image - mean).pow(2)).sum()
+    mean = fst_moment
+    std = torch.sqrt(snd_moment - fst_moment**2)
 
-    std = torch.sqrt(sum_squared_err / pixels_count)
-
-    print("Final Mean:",mean)
-    print("Final Std:",std)
+    print("Final Mean:", mean)
+    print("Final Std:", std)
 
 def one_hot_encode(true_labels: torch.Tensor, num_classes: int, smoothing=0.0):
     assert 0 <= smoothing < 1
